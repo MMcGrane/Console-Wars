875c47b5bbc83cda89d5465ec477f494acddbbed
ArctlcFox <markmcgrane@hotmail.com>
2021-06-09 21:23:34 +0100

    Created a movement script
    Made it so the player could move around!

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
+    public float speed = 50f;
+    public float TurnSpeed = -50f;
+
+    public float Horizontal;
+    public float Vertical;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +18,13 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    { 
         
-        Horizontal = Input.GetAxis("Horizontal");
+         Horizontal = Input.GetAxis("Horizontal");
+         Vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Vertical * speed * Time.deltaTime);
+        transform.Translate(Vector3.left * Horizontal * TurnSpeed * Time.deltaTime);
 
     }
 }
